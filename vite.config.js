3473887804076ac7f3a06c5dafd553c65881f371
itou-create <itou@kcsweb.co.jp>
2025-09-15 19:37:23 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,4 @@
 import { defineConfig } from 'vite'
-<<<<<<< HEAD
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/oyo-guide/'
-=======
-export default defineConfig({
-  base: '/oyo-guide/',   // ← リポ名そのまま
->>>>>>> 064ab302376007c7feccf1541db019684dfbf801
+  base: '/oyo-guide/',
 })
